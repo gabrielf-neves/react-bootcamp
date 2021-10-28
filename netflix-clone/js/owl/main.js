bf9e282e6aa918a2fdc35399a93f783b497e075a
gabrielf-neves <gabrielfernandoneves.contato@gmail.com>
2021-10-28 15:56:10 -0300

    update no segundo desafio

--- netflix-clone/js/owl/main.js
+++ netflix-clone/js/owl/main.js
@@ -1,8 +1,9 @@
 $('.owl-carousel').owlCarousel({
     stagePadding: 50,
     loop:true,
-    margin:10,
-    nav:false,
+    margin:20,
+    nav: false,
+    dots: false,
     responsive:{
         0:{
             items:1
